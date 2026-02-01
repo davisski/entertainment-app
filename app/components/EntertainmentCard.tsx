02e1283681aad33af95cee106d632b3efbcae9e3
Davis Skipsna <skipsna.davis@gmail.com>
2026-02-01 18:51:51 +0200

    #13 - card hover effect

--- app/components/EntertainmentCard.tsx
+++ app/components/EntertainmentCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useTheme } from "../contexts/ThemeContext";
 import dot from "../img/dot.svg";
 import dotLight from "../img/dot-light.svg";
@@ -5,23 +6,31 @@ import bookmarkOutline from "../img/bookmark-outline.svg";
 import tvIcon from "../img/icon-nav-tv-series.svg";
 import navTv from "../img/icon-category-tv.svg";
 import mediumThumbnail from "../img/thumbnails/medium.jpg";
-// import playIcon from "../img/play-icon.svg";
+import playIcon from "../img/play-icon.svg";
+
 export const EntertainmentCard = () => {
     const { isDark } = useTheme();
+    const [isHovered, setIsHovered] = useState(false);
     return (
-        <div className={`h-57.5 justify-between rounded-lg flex flex-col items-center overflow-hidden relative`}>
+        <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className={`h-57.5 justify-between rounded-lg max-w-70 flex flex-col items-center overflow-hidden relative`}>
             <div className="absolute w-8 h-8 z-10 rounded-full top-4 right-4 bg-blue-950 opacity-50 flex items-center justify-center">
                 <img src={bookmarkOutline} alt="Bookmark" />
             </div>
     
             <div className="relative h-43.5 block w-full">
                 <img src={mediumThumbnail} className="w-full z-10 h-43.5 block" alt="Thumbnail" />
-                {/* <div className="absolute w-full h-full z-20 top-0 left-0 bg-black opacity-50 flex items-center justify-center">
-                    <div className="absolute z-30  w-29.25 h-12 bg-white opacity-50 flex items-center justify-evenly gap-4 rounded-full">
-                        <img className="h-7.5 w-7.5" src={playIcon} alt="Play" />
-                        <span>Play</span>
-                    </div>
-                </div> */}
+                {
+                    isHovered && (
+                        <>
+                            <div className="absolute w-full h-full z-20 top-0 left-0 bg-black opacity-50 flex items-center justify-center"></div>
+                            <div className="absolute z-30 w-29.25 h-12 bg-white opacity-50 flex items-center top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] justify-evenly gap-4 rounded-full"></div>
+                            <div className="translate-x-[-50%] translate-y-[-50%] absolute z-40 top-[50%] left-[46%] flex items-center gap-4">
+                                <img className="h-7.5 w-7.5" src={playIcon} alt="Play" />
+                                <span className="text-white">Play</span>
+                            </div>
+                        </>
+                    )
+                }
             </div>
             <div className="flex flex-col w-full">
                 <div className="flex items-center gap-2">
