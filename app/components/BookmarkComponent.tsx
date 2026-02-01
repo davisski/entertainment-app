73824845fd85e9eb816c534981b07d342a306106
Davis Skipsna <skipsna.davis@gmail.com>
2026-02-01 23:08:40 +0200

    #21 - bookmark state change on click

--- app/components/BookmarkComponent.tsx
+++ app/components/BookmarkComponent.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import bookmarkOutline from "../img/bookmark-outline.svg";
+import bookmarkFull from "../img/icon-bookmark-full.svg";
 export const BookmarkComponent = () => {
+  const [isBookmarked, setIsBookmarked] = useState(false);
   return (
-    <div className="absolute w-8 h-8 z-10 rounded-full top-4 right-4 bg-blue-950 opacity-50 flex items-center justify-center">
-        <img src={bookmarkOutline} alt="Bookmark" />
+    <div className="absolute w-8 h-8 z-100 rounded-full top-4 right-4 bg-blue-950 opacity-50 flex items-center justify-center cursor-pointer" onClick={() => setIsBookmarked(!isBookmarked)}>
+        <img src={isBookmarked ? bookmarkFull : bookmarkOutline} alt="Bookmark" />
     </div>
   )
 }
