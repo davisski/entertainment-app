a278b539c06a3f3606a867c892b88a4767589e73
Davis Skipsna <skipsna.davis@gmail.com>
2026-02-08 01:37:50 +0200

    #60 - passing params was the issue

--- app/components/RatingCardComponent.tsx
+++ app/components/RatingCardComponent.tsx
@@ -1,5 +1,5 @@
 export const RatingCardComponent = ({rating}: {rating: number}) => {
-    let formattedRating = rating.toFixed(1);
+    let formattedRating: string = rating > 0 ? rating.toFixed(1) : "N/R";
     return (
         <span className="absolute bottom-2 px-2 py-1 rounded-sm bg-blue-900 right-2 text-red-500">{formattedRating}</span>
     ) 
