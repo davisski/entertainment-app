005604274eceae9f556181824d65a4eb4ac1401b
Davis Skipsna <skipsna.davis@gmail.com>
2026-02-09 22:38:20 +0200

    #82 - link was from trending movies, so if movie is cliked from there, handle that situation

--- app/pages/media/show.tsx
+++ app/pages/media/show.tsx
@@ -1,11 +1,17 @@
 import { useMovies } from "~/contexts/MovieContext";
 import { useSeries } from "~/contexts/SeriesContext";
+import { useTrendingMovies } from "~/contexts/TrendingMovieContext";
 import { MediaShowComponent } from "~/components/MediaShowComponent";
 
 export function Show({media, mediaId, reviews, details}: {media: string; mediaId: string; reviews?: any, details?: any}) {
   if(media === 'movies') {
     const { getMovie } = useMovies();
-    const movie = getMovie(mediaId);
+    const { getTrendingMovie } = useTrendingMovies();
+    let movie = getMovie(mediaId);
+
+    if(!movie) {
+      movie = getTrendingMovie(mediaId);
+    }
     return (
       <MediaShowComponent media={movie} overview={details.overview} reviews={reviews} />
     )
